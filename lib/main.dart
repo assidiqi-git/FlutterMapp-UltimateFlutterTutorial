69daf1da448f3bc1e1aa1c862f126de87c6ed550
Assidiqi <epiphania.rino@kemenkeu.go.id>
2025-10-27 14:13:56 +0800

    section - value notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/views/widget_tree.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,62 +18,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter'),
-        centerTitle: true,
-        backgroundColor: Colors.teal,
-      ),
-      body: currentIndex == 0
-          ? Center(child: Text('Home'))
-          : Center(child: Text('Profile')),
-      drawer: SafeArea(
-        child: Drawer(
-          child: Column(
-            children: [
-              DrawerHeader(child: Text('Drawer Header')),
-              ListTile(title: Text('Logout')),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
-        spacing: 20,
-        children: [
-          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-        ],
-      ),
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        selectedIndex: currentIndex,
-      ),
+      home: WidgetTree(),
     );
   }
 }
---
+++ lib/views/widget_tree.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/data/notifiers.dart';
+import 'package:flutter_app/views/pages/home_pages.dart';
+import 'package:flutter_app/views/pages/profile_pages.dart';
+
+import 'widgets/navbar_widget.dart';
+
+List<Widget> pages = [HomePages(), ProfilePages()];
+
+class WidgetTree extends StatelessWidget {
+  const WidgetTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+      drawer: SafeArea(
+        child: Drawer(
+          child: Column(
+            children: [
+              DrawerHeader(child: Text('Drawer Header')),
+              ListTile(title: Text('Logout')),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        spacing: 20,
+        children: [
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+        ],
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (context, selectedPage, child) {
+          return pages.elementAt(selectedPage);
+        },
+      ),
+      bottomNavigationBar: NavbarWidget(),
+    );
+  }
+}
