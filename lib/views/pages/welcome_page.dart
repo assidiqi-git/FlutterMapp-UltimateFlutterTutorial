2753cb0b81314e2f0f2a47ac9d7cb8f2e359e332
Assidiqi <epiphania.rino@kemenkeu.go.id>
2025-10-28 15:45:53 +0800

    add lottie

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/views/widget_tree.dart';
 import 'package:flutter_app/views/widgets/hero_widget.dart';
+import 'package:lottie/lottie.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -13,7 +14,18 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            HeroWidget(),
+            Lottie.asset('assets/lotties/welcome.json'),
+            FittedBox(
+              child: Text(
+                'House Like Home',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 500,
+                  letterSpacing: 30,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
             FilledButton(
               onPressed: () {
                 Navigator.pushReplacement(
@@ -25,6 +37,25 @@ class WelcomePage extends StatelessWidget {
                   ),
                 );
               },
+              style: FilledButton.styleFrom(
+                minimumSize: Size(double.infinity, 40),
+              ),
+              child: Text('Get Started'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WidgetTree();
+                    },
+                  ),
+                );
+              },
+              style: FilledButton.styleFrom(
+                minimumSize: Size(double.infinity, 40),
+              ),
               child: Text('Login'),
             ),
           ],
