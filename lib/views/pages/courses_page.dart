810fc7e98159bfc863e7ce96c0b9d309b4980cab
Assidiqi <epiphania.rino@kemenkeu.go.id>
2025-11-12 18:15:25 +0800

    finish

--- lib/views/pages/courses_page.dart
+++ lib/views/pages/courses_page.dart
@@ -2,6 +2,8 @@ import 'dart:convert' as convert;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app/data/classes/activity_class.dart';
+import 'package:flutter_app/views/widgets/container_widget.dart';
+import 'package:flutter_app/views/widgets/hero_widget.dart';
 import 'package:http/http.dart' as http;
 
 class CoursesPage extends StatefulWidget {
@@ -12,21 +14,19 @@ class CoursesPage extends StatefulWidget {
 }
 
 class _CoursesPageState extends State<CoursesPage> {
-  late Activity activity;
-
   @override
   void initState() {
     getData();
     super.initState();
   }
 
-  void getData() async {
+  Future getData() async {
     var url = Uri.https('bored-api.appbrewery.com', '/random');
 
     // Await the http get response, then decode the json-formatted response.
     var response = await http.get(url);
     if (response.statusCode == 200) {
-      activity = Activity.fromJson(
+      return Activity.fromJson(
         convert.jsonDecode(response.body) as Map<String, dynamic>,
       );
     } else {
@@ -38,6 +38,33 @@ class _CoursesPageState extends State<CoursesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: Column());
+    return Scaffold(
+      appBar: AppBar(),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasData) {
+            Activity activity = snapshot.data;
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  ContainerWidget(
+                    title: activity.activity,
+                    description: activity.accessibility,
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Center(child: Text('Error'));
+          }
+        },
+      ),
+    );
   }
 }
